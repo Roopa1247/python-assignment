983a48968d9ecb6eaed5c860943ae054f5071690
Roopa1247 <roopas1247@gmail.com>
2024-06-04 10:47:04 +0530

    Add files via upload

---
+++ input_output2.py
@@ -0,0 +1,29 @@
+"""
+Write a Python program that asks the user to enter their name using the input() function, and then prints a greeting message using the print() function. For example, if the user enters "Alice", the program should print "Hello, Alice!"
+"""
+print('hello,'+input("enter name"))
+"""
+WAP to print the following output in single of code
+'Hello' : dynamic using input()
+'Anil' : dynamic using input()
+Note: please use print() to combine the output
+Final output format:
+Hello Anil
+Hi Anil
+I Love Python
+"""
+print(input("enter greet"),"Anil")
+print("hii",input("enter name"))
+print("i love python")
+
+
+
+
+
+"""
+Write a script that asks the user for their name, age, and favorite color, and then prints a single sentence summarizing this information in a formatted string.
+"""
+name=input("enter name")
+age=input("enter age")
+fav_color=input("enter fav colour")
+print(name,age,fav_color)
---
+++ variables.py
@@ -0,0 +1,29 @@
+"""
+Assignment-1:
+WAP to assign 3 values (string, int, float)
+to 3 variables (Student_name, Roll_Number, Percentage_of_marks).
+print the values using print function.
+"""
+student_name ='roopa'
+Roll_Number=32
+Percentage_of_marks=90.5
+print(student_name)
+print(Roll_Number)
+print(Percentage_of_marks)
+
+"""
+Assignment-2:
+WAP to assign value 6 to two different variables (example: a1, b1) 
+using single line of code.
+Check the variable values and memory address.
+"""
+a1,b1=6,6
+print(a1,b1)
+print(id(a1),id(b1))
+"""
+Assignment-3:
+WAP to assign multiple values to multiple variables
+in single line of code
+"""
+a,b,c,d=1,2,3,4
+print(a,b,c,d)
